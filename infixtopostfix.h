c903131914e9461fa54392e4b6586cd5b8024cff
kdbg3k <112708143+kdbg3k@users.noreply.github.com>
2023-04-25 20:36:15 -0500

    Update infixtopostfix.h

--- infixtopostfix.h
+++ infixtopostfix.h
@@ -4,10 +4,10 @@
 #include <iostream>
 
 
-bool is_balanced(std::string expression);
+bool is_balanced(std::string expression); //returns true if expression has balanced parentheses, otherwise returns false
 
-std::string infixToPostfix(std::string s);
+std::string infixToPostfix(std::string s); //accepts string as infix expression and converts it to postfix expression and returns result
 
-bool is_operator(char c);
+bool is_operator(char c); //checks if character is an operator
 
-int get_precedence(char c);
+int get_precedence(char c); //gets precedence of character
